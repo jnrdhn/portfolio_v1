6f26106d4dadf314d96a16351d9210585bbcac24
Janardhan Edara <nick149e@gmail.com>
2025-01-14 00:06:19 +0530

    animated scrollintoview, css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,62 @@
-import React from 'react';
-import { useTheme } from '../context/ThemeContext';
-import '../styles/Navbar.css';
+import React from "react";
+import { useTheme } from "../context/ThemeContext";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   const { darkMode, toggleTheme } = useTheme();
 
   return (
     <div className="navbar-wrapper">
-      <nav className={`navbar ${darkMode ? 'dark' : ''}`}>
+      <nav className={`navbar ${darkMode ? "dark" : ""}`}>
         <div className="navbar-container">
           <div className="navbar-buttons">
-            <button onClick={() => document.getElementById('about').scrollIntoView()}>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("about")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
               <i className="fas fa-user"></i>
             </button>
-            <button onClick={() => document.getElementById('projects').scrollIntoView()}>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("projects")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
               <i className="fas fa-code"></i>
             </button>
-            <button onClick={() => document.getElementById('experience').scrollIntoView()}>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("experience")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
               <i className="fas fa-briefcase"></i>
             </button>
-            <button onClick={() => document.getElementById('education').scrollIntoView()}>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("education")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
               <i className="fas fa-graduation-cap"></i>
             </button>
-            <button onClick={() => document.getElementById('contact').scrollIntoView()}>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("contact")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
+            >
               <i className="fas fa-envelope"></i>
             </button>
             <button onClick={toggleTheme}>
-              <i className={`fas ${darkMode ? 'fa-sun' : 'fa-moon'}`}></i>
+              <i className={`fas ${darkMode ? "fa-sun" : "fa-moon"}`}></i>
             </button>
           </div>
         </div>
--- src/data/education.js
+++ src/data/education.js
@@ -2,101 +2,17 @@
 const education = [
   {
     id: 1,
-    institution: "University Name sada",
-    degree: "Bachelor of Science",
-    field: "Computer Science",
-    startDate: "2016",
-    endDate: "2020",
-    logo: "/images/education/university1.png",
+    institution: "Indian Institute of Information Technology",
+    degree: "Bachelor of Technology",
+    field: "Computer of Computer Science & Engineering",
+    startDate: "2019",
+    endDate: "2023",
+    logo: "/images/test.jpg",
     description: "Description of studies and experience",
     achievements: [
-      "Dean's List 2018-2020",
-      "First Class Honours",
-      "Best Final Year Project"
-    ],
-    relevantCourses: [
-      "Data Structures",
-      "Algorithms",
-      "Web Development",
-      "Database Systems"
-    ]
-  },
-  {
-    id: 2,
-    institution: "University fgdsgsddfg ",
-    degree: "Bachelor ",
-    field: "Computer ",
-    startDate: "2016",
-    endDate: "2020",
-    logo: "/images/education/university1.png",
-    description: "Description of studies and experience",
-    achievements: [
-      "Dean's List 2018-2020",
-      "First Class Honours",
-      "Best Final Year Project"
-    ],
-    relevantCourses: [
-      "Data Structures",
-      "Algorithms",
-      "Web Development",
-      "Database Systems"
-    ]
-  },
-  {
-    id: 1,
-    institution: "University Name    dfgsdg",
-    degree: "Bachelor of Science",
-    field: "Computer Science",
-    startDate: "2016",
-    endDate: "2020",
-    logo: "/images/education/university1.png",
-    description: "Description of studies and experience",
-    achievements: [
-      "Dean's List 2018-2020",
-      "First Class Honours",
-      "Best Final Year Project"
-    ],
-    relevantCourses: [
-      "Data Structures",
-      "Algorithms",
-      "Web Development",
-      "Database Systems"
-    ]
-  },
-  {
-    id: 1,
-    institution: "University Name wer were",
-    degree: "Bachelor of Science dfgdsfgsd",
-    field: "Computer Science",
-    startDate: "2016",
-    endDate: "2020",
-    logo: "/images/education/university1.png",
-    description: "Description of studies and experience",
-    achievements: [
-      "Dean's List 2018-2020",
-      "First Class Honours",
-      "Best Final Year Project"
-    ],
-    relevantCourses: [
-      "Data Structures",
-      "Algorithms",
-      "Web Development",
-      "Database Systems"
-    ]
-  },
-  {
-    id: 1,
-    institution: "University Name wwerwrwer",
-    degree: "Bachelor of Science hjhkuu",
-    field: "Computer Science",
-    startDate: "2016",
-    endDate: "2020",
-    logo: "/images/education/university1.png",
-    description: "Description of studies and experience",
-    achievements: [
-      "Dean's List 2018-2020",
-      "First Class Honours",
-      "Best Final Year Project"
+      "",
+      "",
+      ""
     ],
     relevantCourses: [
       "Data Structures",
